d29ed1bac7d7b3d32953e30e297222b170862325
Edelzio Lopes <edelzio@hotmail.com>
2025-08-25 12:37:59 -0300

    1.5

--- Application/controllers/Usuario.php
+++ Application/controllers/Usuario.php
@@ -2,11 +2,29 @@
 
 use Application\core\Controller;
 
-class Usuario extends Controller
+class Usuarios extends Controller
 {
   public function index()
   {
-    $this->view('usuario/index');
+    $Generos = $this->model('Generos');
+    $data = $Generos::listarTudo();
+    $this->view('genero/index', ['generos' => $data]);
   }
+
+  public function salvar()
+  {
+    $nome = $_POST['txt_nome'];
+
+    $Generos = $this->model('Generos');
+    $Generos::salvar($nome);
+    $this->redirect('genero/index');
+  } 
+  
+    public function excluir($id)
+    {
+      $Generos = $this->model('Generos');
+      $Generos::excluir($id);
+      $this->redirect('genero/index');
+    }  
 
 }
