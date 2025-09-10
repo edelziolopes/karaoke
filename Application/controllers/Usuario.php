7f7e1d13cc1fedd4748d2f4aa2d13fff128a778f
Edelzio Lopes <edelzio@hotmail.com>
2025-09-10 13:40:36 -0300

    2.5

--- Application/controllers/Usuario.php
+++ Application/controllers/Usuario.php
@@ -23,6 +23,28 @@ class Usuario extends Controller
     $this->redirect('usuario/index');
   } 
   
+  public function cadastro()
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = $_POST['txt_nome'];
+    $email = $_POST['txt_email'];
+    $senha = $_POST['txt_senha'];
+    $turma = $_POST['txt_turma'];
+    $foto = $FILES['txt_foto'];
+        
+    $timestamp = date('YmdHis');
+    $fotoName = $timestamp . '.jpg';
+    $uploadPath = '../public/fotos/' . $fotoName;
+    if (move_uploaded_file($foto['tmp_name'], $uploadPath)) {
+      $Usuarios = $this->model('Usuarios');
+      $Usuarios::salvar($nome, $email, $senha, $turma, $fotoName);
+      $this->redirect('home/login');
+    }} else {
+      $this->view('home/cadastro');
+    }
+  } 
+
+  
     public function excluir($id)
     {
       $Usuarios = $this->model('Usuarios');
@@ -49,9 +71,5 @@ class Usuario extends Controller
     setcookie('usuario', '', time() - 3600, '/');
     $this->redirect('home');
   }  
-  public function cadastro()
-  {
-    $this->view('usuario/cadastro');
-  }
 
 }
